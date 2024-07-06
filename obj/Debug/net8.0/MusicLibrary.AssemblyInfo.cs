aa0593283d1dad034aeb821704d1f8f528cb1e02
DevLost - PC <gabi_tlc@yahoo.com>
2024-07-06 21:12:49 +0300

    Updated tamplate to use web api instead of mvc

--- obj/Debug/net8.0/MusicLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f191a420ec80323660023bff1a31b5b963eef79")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
